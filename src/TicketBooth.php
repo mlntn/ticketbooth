e2bb686992040227dd544ececc97d682cb925a5c
Jared Mellentine <jared@mellentine.com>
2017-07-18 09:36:09 -0700

    First stab

---
+++ src/TicketBooth.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Mlntn;
+
+class TicketBooth
+{
+
+    /**
+     * @var string
+     */
+    protected $path;
+
+    /**
+     * @var string
+     */
+    protected $file;
+
+    /**
+     * @var int
+     */
+    protected $time_limit;
+
+    public function __construct($time_limit = 60, $path = '.', $file = '.ticket')
+    {
+        $this->path = $path;
+        $this->file = $file;
+        $this->time_limit = $time_limit;
+    }
+
+    /**
+     * @throws TicketException
+     */
+    public function claim()
+    {
+        if ($this->check()) {
+            $this->renew();
+        }
+        else {
+            throw new TicketException("Another process has the ticket right now");
+        }
+    }
+
+    public function renew()
+    {
+        touch($this->getFilePath());
+    }
+
+    /**
+     * @return bool
+     */
+    public function check()
+    {
+        return $this->getFileTime() + $this->time_limit < time();
+    }
+
+    /**
+     * @return int
+     */
+    protected function getFileTime()
+    {
+        $file = $this->getFilePath();
+
+        if (file_exists($file)) {
+            return filemtime($file);
+        }
+
+        return 0;
+    }
+
+    /**
+     * @return string
+     */
+    protected function getFilePath()
+    {
+        return realpath($this->path) . '/' . $this->file;
+    }
+
+}
---
+++ src/TicketException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Mlntn;
+
+class TicketException extends Exception
+{
+
+}
